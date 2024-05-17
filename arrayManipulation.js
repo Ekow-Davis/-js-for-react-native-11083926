d75a8a3a5c3b452d3eb4d538704d9efdbc291bbc
Ekow-Davis <paakowdavis@gmail.com>
2024-05-17 16:47:19 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,6 +1,9 @@
-//Task 1
+//Task 1 
+/*This task is to manipulate an array by checking each value if it is perfectly divisible by 2
+the after if true it sqaures if not it multiplies by three*/
 
 const inputArray = [1, 2, 3, 4, 5];
+const stringArray = ['Bat', 'cat', 'HAT', 'Mat', 'rat']
 
 function processArray (array) {
     return array.map(number => number % 2 === 0 ? number ** 2: number *3)
@@ -11,6 +14,16 @@ const processedArray = processArray(inputArray);
 console.log(processedArray);
 
 
-/* function arrayInput() {
-    const input = prompt
-} */
+//Task 2
+/* This task is to take in two arrays as arguments and then modifies the elemets in the stringArray based on
+the output we get in the processedArray. If even we capitalise, if odd we turn teh string into lowercase */
+
+function formatArrayStrings (stringArray, processedArray) {
+    return stringArray.map((string, index) => {
+        const number = processedArray[index];
+        return number % 2 === 0 ? string.toUpperCase() : string.toLowerCase();
+    })
+}
+
+const formattedStrings = formatArrayStrings(stringArray, processedArray);
+console.log(formattedStrings);
